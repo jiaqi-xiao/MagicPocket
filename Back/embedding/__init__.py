3173b83a331d0661e290a3cec501802f3991d537
Marco-Ray <wangzehuan2021@outlook.com>
2024-09-27 16:55:54 +0800

    single record embedding | cluster

--- Back/embedding/__init__.py
+++ Back/embedding/__init__.py
@@ -1,54 +1,54 @@
 from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
 from langchain_huggingface import HuggingFaceEmbeddings
+import numpy as np
 
 
 class Model:
     def __init__(self):
         self.index = None
         # 创建 Hugging Face 嵌入模型实例
-        self.embeddings = HuggingFaceEmbeddings(
+        self.embeddingsModel = HuggingFaceEmbeddings(
             model_name="sentence-transformers/all-MiniLM-L6-v2"
         )
 
-    def embedding(self, recordsList: list):
+    def embedding(self, record):
 
-        # 将文本转换为 Document 对象
-        documents = [Document(page_content=record["context"]) for record in recordsList]
+        context_v = np.array(self.embeddingsModel.embed_query(record.context))
+        if record.comment:
+            comment_v = np.array(self.embeddingsModel.embed_query(record.comment))
+        return (
+            self.vector_operation(context_v, comment_v)
+            if record.comment
+            else context_v
+        )
 
-        # 使用 FAISS 存储嵌入向量，使用 from_documents 方法
-        self.index = FAISS.from_documents(documents, self.embeddings)
+    def vector_operation(self, context_v, comment_v):
+        return (context_v + comment_v).tolist()
 
-    # 获取索引中所有嵌入的向量
-    def get_all_vectors(self):
-        n_vectors = self.index.index.ntotal  # 获取索引中向量的总数
-        vectors = self.index.index.reconstruct_n(0, n_vectors)  # 获取所有嵌入向量
-        return vectors
+    # # 获取索引中所有嵌入的向量
+    # def get_all_vectors(self):
+    #     n_vectors = self.index.index.ntotal  # 获取索引中向量的总数
+    #     vectors = self.index.index.reconstruct_n(0, n_vectors)  # 获取所有嵌入向量
+    #     return vectors
 
-    def RAG(self, query: str, k: int = 3):
-        # 定义查询文本
+    # def RAG(self, query: str, k: int = 3):
+    #     # 定义查询文本
 
-        # 执行相似性搜索，返回最相似的前 k 个结果
-        results = self.index.similarity_search(query, k=k)
+    #     # 执行相似性搜索，返回最相似的前 k 个结果
+    #     results = self.index.similarity_search(query, k=k)
 
-        # 输出结果
-        for result in results:
-            print("相似文本：", result.page_content)
+    #     # 输出结果
+    #     for result in results:
+    #         print("相似文本：", result.page_content)
 
-        return results
+    #     return results
 
 
 if __name__ == "__main__":
-    # 定义文本列表
-    recordsList = {
-        "data": [
-            {"id": 0, "comment": "string", "context": "string"},
-            {"id": 1, "comment": "string1", "context": "string1"},
-        ]
-    }
+    record = {"id": 0, "comment": "", "context": "string"}
 
     model = Model()
-    model.embedding(recordsList["data"])
-    vectors = model.get_all_vectors()
+    vector = model.embedding(record)
 
-    print(vectors)
+    print(vector)
--- Back/main.py
+++ Back/main.py
@@ -1,5 +1,5 @@
 # TODO Back-end
-'''
+"""
 数据结构
 
 每条record
@@ -24,18 +24,28 @@
         }
     ]
 }
-'''
+"""
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Hello World!"
+
+
+import embedding as e
+
+# 初始化大模型
+model = e.Model()
 
 class Record(BaseModel):
     id: int
     comment: str | None = None
     context: str
 
-class RecordsList(BaseModel):
-    data: list[Record]
 
 class RecordwithVector(BaseModel):
     id: int
@@ -43,42 +53,25 @@ class RecordwithVector(BaseModel):
     context: str
     vector: list[float]
 
-class RecordsListwithVector(BaseModel):
-    data: list[RecordwithVector]
-
-app = FastAPI()
-
-import embedding as e
-
-@app.get("/")
-async def root():
-    return "Hello World!"
 
 @app.post("/embedding/")
-async def generate(recordsList: RecordsList):
-    recordsList_dict = recordsList.model_dump()
-    model = e.Model()
-
-    # 对记录数据生成嵌入
-    model.embedding(recordsList_dict["data"])
-
-    # 获取生成的嵌入向量
-    vectors = model.get_all_vectors()
-
-    # 确保记录数和向量数匹配
-    if len(vectors) != len(recordsList_dict["data"]):
-        return {"error": "向量数与记录数不匹配"}
-
-    # 更新 recordsList 中每条 record，添加 vector
-    updated_records = []
-    for record, vector in zip(recordsList_dict["data"], vectors):
-        updated_record = RecordwithVector(
-            id=record['id'],
-            comment=record.get('comment'),
-            context=record['context'],
-            vector=vector
-        )
-        updated_records.append(updated_record)
+async def embedding_single_record(record: Record):
+    # 对记录数据生成嵌入，并获取生成的嵌入向量
+    vector = model.embedding(record)
 
     # 返回带有向量的记录列表
-    return RecordsListwithVector(data=updated_records)
+    return RecordwithVector(
+        id=record.id,
+        comment=record.comment,
+        context=record.context,
+        vector=vector,
+    )
+
+import cluster
+
+class RecordsListWithVector(BaseModel):
+    data: list[RecordwithVector]
+
+@app.post("/cluster/")
+async def hierarcy_cluster(recordsList: RecordsListWithVector):
+    cluster.hierarcy_clustering(recordsList.data)
