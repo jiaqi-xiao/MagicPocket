bff1106bae1fa0d4df0ee1783c571d4e7c624ad6
Zehuan <wangzehuan2021@outlook.com>
2024-10-14 13:04:25 +0800

    style:keep /cluster while create two api a more uniform style

--- Back/extractModule/__init__.py
+++ Back/extractModule/__init__.py
@@ -3,6 +3,7 @@ from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 import os
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
+from typing import Union
 
 # openai api key
 if "OPENAI_API_KEY" in os.environ:
@@ -16,17 +17,20 @@ model = "gpt-4o-mini"
 
 # Define a Pydantic model for individual intents
 class Intent(BaseModel):
-    recordId: str
+    id: int
     intent: str
 
 # Define a Pydantic model for grouped intents
 class GroupedIntents(BaseModel):
-    group: list[str]
-    highLevelIntent: str
+    id: int
+    child: list[Intent]
+    child_num: int
+    priority: int
+    intent: str
 
 # Define the main Pydantic model
 class ExtractIntents(BaseModel):
-    extractedIntents: list[Intent]
+    extractIntens: list[Intent]
     groupedIntents: list[GroupedIntents]
 
 # direct
@@ -34,11 +38,11 @@ class ExtractModelDirect:
     def __init__(self):
         self.instruction = '''
         ## System:
-        根据提供的record列表，提取并总结用户的意图。
-        
-        - 提取每个record中的意图，保证表述风格统一，每个意图不超过7个词。
-        - 将所有意图分组，确保组间差异尽可能大。
-        - 从每个组中提炼出一个更高级的意图。
+        根据提供的record列表，提取并总结用户的意图。  
+            - 提取每个record中的意图，保证表述风格统一，每个意图不超过7个词。
+            - 将所有意图分组，确保组间差异尽可能大。
+            - 从每个组中提炼出一个更高级的意图。
+            - 推理出用户对每组意图的优先级（数字越大代表优先级越低，用整数表示）
         
         # Steps
         1. **Analyze Each Record:** 
@@ -49,6 +53,8 @@ class ExtractModelDirect:
             - Organize the extracted intents into distinct groups where the differences between the groups are maximized.
         4. **Derive High-level Intents:**
             - For each group, derive a higher-level intent that encompasses the primary theme or objective of that group.
+        5. **Deduce the user's priority:**
+            - Consider all groups and deduce the user's priority for each group and rank them according to the priority
             
         # Output Format
             - The output should be structured in JSON format as following {format_instructions}.
@@ -79,16 +85,48 @@ class ExtractModelDirect:
             self.chain_direct.invoke({"records": records})
         )
 
+
 # cluster-based
 class ExtractModelCluster:
     def __init__(self):
         # self.instruction_low_intent_extraction_cn = '从用户提供的多条记录中提取一个简明的意图，将其限制在7个词以内。每条记录包括选中的文本、对应的上下文和注释。基于这些信息，提取出最能反映意图的短语。\n\n# Steps\n\n1. 阅读并理解每条记录的选中的文本、上下文和注释。\n2. 将所有记录的主要意图归纳整理。\n3. 从归纳的结果中提取出一个简明的意图，限制在不超过7个词。\n\n# Output Format\n\n生成的意图应以不超过7个词的短语形式呈现，仅输出意图文本。\n\n# Examples\n\n**记录 1**\n- 选中文本: "请在下周五前提交报告"\n- 上下文: "公司正在收集今年的年度资料..."\n- 注释: "疑问：报告的截止日期是否可以延期？"\n\n**提取的意图**\n- "询问报告延期可能性"\n\n# Notes\n\n- 确保意图清晰明了，并能充分反映记录的核心信息。\n- 对于模棱两可的信息，请根据上下文和注释中的线索进行推测。'
         # self.instruction_high_intent_extraction_cn = '从提供的多个低级意图中提炼出一个高级意图，限制在7个词以内。\n\n# Steps\n\n1. 理解和分析所有提供的低级意图。\n2. 识别这些低级意图之间的共同主题或目的。\n3. 将这些共有的主题或目的浓缩成一个高级意图。\n4. 确保高级意图不超过7个词。\n\n# Output Format\n\n  生成的意图应以不超过7个词的短语形式呈现，仅输出高级意图文本\n\n# Examples\n\n- 低级意图: ["购买书籍", "在线订购", "寻找最佳价格"]\n  - 高级意图: "在线购买书籍"\n\n- 低级意图: ["预约医生", "查找最近诊所", "拨打医生电话"]\n  - 高级意图: "安排医生预约"\n\n# Notes\n\n- 高级意图应尽可能涵盖所有提供的低级意图。\n- 语言应简练且易于理解。'
-        self.instruction = "根据用户提供的数据（record列表或intent列表），提取和总结用户意图或对意图进行分组和提炼。\n\n- 当用户提供record列表时，每个record包含选中文本、上下文及注释，所有records具有相似的意图。\n- 提取和总结这些records的意图，保证意图描述风格统一，每个意图不超过7个词。\n- 当用户提供intent列表时，提炼出一个更高级别的意图。\n\n# Steps \n\n1. **Record列表处理**:\n   - 阅读每个record内的选中文本、上下文及注释。\n   - 理解这些record的共同意图。\n   - 提取并用不超过7个词总结统一的意图。\n\n2. **Intent列表处理**:\n   - 为每个intent组提炼出一个更高级别的意图。\n\n# Output Format\n\n在处理record列表时，输出每个record列表中提取的意图，限制在不超过7个词。  \n在处理intent列表时，为每个组提供一个提炼后的高级别意图。\n\n# Examples\n\n- **Record列表示例**:\n  - 输入: \n    - 选择的文本: \"在家工作使我更高效\"\n    - 上下文: \"与传统办公室环境相比\"\n    - 用户评论: \"灵活性带来效率\"\n  - 输出: 更高效工作的灵活性\n\n- **Intent列表示例**:\n  - 输入: [\"提高效率\"，\"增加时间灵活性\"]\n  - 输出: 高效与灵活性\n\n# Notes\n\n- 确保每个提取或提炼的意图表达明确且符合原意。\n- 在意图提炼时，注意使用简练的语言以保持表达简洁清晰。"
+        self.instruction = '''
+        ## System:
+        根据用户提供的数据（record列表或intent列表），提取和总结用户意图或对意图进行分组和提炼。
+            - 当用户提供record列表时，每个record包含选中文本、上下文及注释，所有records语义相似。
+            - 从这些records中提取和总结一个且仅有一个意图，这个意图不超过7个词。
+            - 当用户提供intent列表时，提炼出一个更高级别的意图。
+        
+        # Steps
+            1. **Record列表处理**:
+                - 阅读每个record内的选中文本、上下文及注释。
+                - 理解这些record的共同意图。
+                - 提取并用不超过7个词总结这个共同意图。
+            2. **Intent列表处理**:
+                - 为每个intent组提炼出一个更高级别的意图。
+        
+        # Output Format
+            在处理record列表时，输出每个record列表中提取的意图，限制在不超过7个词。
+            在处理intent列表时，为每个组提供一个提炼后的高级别意图。
+            - The output should be structured in JSON format as following {format_instructions}.
+        
+        # Notes
+            - 确保每个提取或提炼的意图表达明确且符合原意。
+            - 在意图提炼时，注意使用简练的语言以保持表达简洁清晰。
+        
+        ## User:
+        {records}
+        '''
 
 
         self.model = ChatOpenAI(model=model)
-        self.parser = StrOutputParser()
+        self.parser = JsonOutputParser(pydantic_object=Intent)
+        self.prompt_template = PromptTemplate(
+            template=self.instruction,
+            input_variables=['records'],
+            partial_variables={"format_instructions": self.parser.get_format_instructions()},
+        )
 
         # self.prompt_template_low_intent = ChatPromptTemplate.from_messages(
         #     [("system", self.instruction_low_intent_extraction_cn), ("user", "{records}")]
@@ -96,10 +134,6 @@ class ExtractModelCluster:
         # self.prompt_template_high_intent = ChatPromptTemplate.from_messages(
         #     [("system", self.instruction_high_intent_extraction_cn), ("user", "{records}")]
         # )
-
-        self.prompt_template = ChatPromptTemplate.from_messages(
-            [('system', self.instruction), ('user', "{records}")]
-        )
 
         # self.chain_low_intent = (
         #     self.prompt_template_low_intent | self.model | self.parser
@@ -111,7 +145,7 @@ class ExtractModelCluster:
             self.prompt_template | self.model | self.parser
         )
 
-    async def invoke(self, records, mode="h"):
+    async def invoke(self, records):
         return (
             # self.chain_low_intent.invoke({"records": records})
             # if mode != "h"
--- Back/main.py
+++ Back/main.py
@@ -141,19 +141,19 @@ import extractModule
 
 extractModelCluster = extractModule.ExtractModelCluster()
 
-@app.post("/cluster/")
+@app.post("/extract/cluster/")
 async def hierarcy_cluster(
     recordsList: RecordsListWithVector,
     distance_threshold: float = 0.5,
-    level: int | None = Query(ge=1),
-    intent_num: int | None = Query(ge=1),
+    level: int = Query(ge=1),
+    intent_num: int = Query(ge=1),
 ):
     root = [record.model_dump() for record in recordsList.data]
     count = len(root)
 
     newRoot = []
     if count < 2:
-        recordsCluster = "**记录1**\n- 选中文本: {}\n- 上下文: {}\n- 注释: {}".format(
+        recordsCluster = "[**记录1**\n- 选中文本: {}\n- 上下文: {}\n- 注释: {}]".format(
             root[0]["content"], root[0]["context"], root[0]["comment"]
         )
         intent = await extractModelCluster.invoke(recordsCluster)
@@ -171,8 +171,7 @@ async def hierarcy_cluster(
         ]
     hc_tree = clusterGenerator.hierarcy_clustering(root, distance_threshold)
     for key, c in hc_tree.items():
-        recordsCluster = "\n\n".join(
-            [
+        recordsCluster = [
                 "**记录{}**\n- 选中文本: {}\n- 上下文: {}\n- 注释: {}".format(
                     index + 1,
                     root[index]["comment"],
@@ -181,13 +180,12 @@ async def hierarcy_cluster(
                 )
                 for index in c
             ]
-        )
         intent = await extractModelCluster.invoke(recordsCluster)
-        intent_v = model.embedding({"intent": intent}, ["intent"])
+        intent_v = model.embedding(intent, ["intent"])
         newRoot.append(
             {
                 "id": count + int(key),
-                "intent": intent,
+                "intent": intent['intent'],
                 "vector": intent_v,
                 "priority": 5,
                 "child_num": len(c),
@@ -204,24 +202,22 @@ async def hierarcy_cluster(
     while i < level:
         i += 1
         if len(root) <= intent_num:
-            return root
+            return [{key: node[key] for key in node if key != "vector"} for node in root]
         newRoot = []
         hc_tree = clusterGenerator.hierarcy_clustering(root, distance_threshold)
         for key, c in hc_tree.items():
             if len(c) == 1:
                 newRoot.append(root[c[0]])
             else:
-                intentsCluster = "- 意图: {}".format(
-                    [root[index]["intent"] for index in c]
-                )
+                intentsCluster = [root[index]["intent"] for index in c]
                 intent = await extractModelCluster.invoke(intentsCluster)
-                intent_v = model.embedding({"intent": intent}, ["intent"])
+                intent_v = model.embedding(intent, ["intent"])
                 newRoot.append(
                     {
                         "id": count + int(key),
-                        "intent": intent,
+                        "intent": intent['intent'],
                         "vector": intent_v,
-                        "priority": 5,
+                        "priority": 1,
                         "child_num": len(c),
                         "child": [
                             {
@@ -240,10 +236,10 @@ async def hierarcy_cluster(
 
 extractModelDirect = extractModule.ExtractModelDirect()
 
-@app.post("/extract_direct/")
+@app.post("/extract/direct/")
 async def direct_extract_intent(
     recordsList: RecordsList,
-):
+):  
     root = [record.model_dump() for record in recordsList.data]
 
     recordsCluster = "\n\n".join(
@@ -257,5 +253,120 @@ async def direct_extract_intent(
                 for index in range(len(root))
             ]
         )
-    
-    return await extractModelDirect.invoke(recordsCluster)
+    groupedIntents = await extractModelDirect.invoke(recordsCluster)
+    return groupedIntents["groupedIntents"]
+
+@app.post("/cluster/")
+# async def direct_extract_intent(
+#     recordsList: RecordsListWithVector,
+#     distance_threshold: float = 0.5,
+#     level: int = Query(ge=1),
+#     intent_num: int = Query(ge=1),
+# ):  
+#     root = [record.model_dump() for record in recordsList.data]
+
+#     recordsCluster = "\n\n".join(
+#             [
+#                 "**记录{}**\n- 选中文本: {}\n- 上下文: {}\n- 注释: {}".format(
+#                     index + 1,
+#                     root[index]["content"],
+#                     root[index]["content"],
+#                     root[index]["comment"],
+#                 )
+#                 for index in range(len(root))
+#             ]
+#         )
+#     groupedIntents = await extractModelDirect.invoke(recordsCluster)
+#     return groupedIntents["groupedIntents"]
+
+async def hierarcy_cluster(
+    recordsList: RecordsListWithVector,
+    distance_threshold: float = 0.5,
+    level: int = Query(ge=1),
+    intent_num: int = Query(ge=1),
+):
+    root = [record.model_dump() for record in recordsList.data]
+    count = len(root)
+
+    newRoot = []
+    if count < 2:
+        recordsCluster = "[**记录1**\n- 选中文本: {}\n- 上下文: {}\n- 注释: {}]".format(
+            root[0]["content"], root[0]["context"], root[0]["comment"]
+        )
+        intent = await extractModelCluster.invoke(recordsCluster)
+        return [
+            {
+                "id": 0,
+                "intent": intent,
+                "priority": 5,
+                "child_num": len(c),
+                "child": [
+                    {key: root[index][key] for key in root[index] if key != "vector"}
+                    for index in c
+                ],
+            }
+        ]
+    hc_tree = clusterGenerator.hierarcy_clustering(root, distance_threshold)
+    for key, c in hc_tree.items():
+        recordsCluster = [
+                "**记录{}**\n- 选中文本: {}\n- 上下文: {}\n- 注释: {}".format(
+                    index + 1,
+                    root[index]["comment"],
+                    root[index]["content"],
+                    root[index]["context"],
+                )
+                for index in c
+            ]
+        intent = await extractModelCluster.invoke(recordsCluster)
+        intent_v = model.embedding(intent, ["intent"])
+        newRoot.append(
+            {
+                "id": count + int(key),
+                "intent": intent['intent'],
+                "vector": intent_v,
+                "priority": 5,
+                "child_num": len(c),
+                "child": [
+                    {key: root[index][key] for key in root[index] if key != "vector"}
+                    for index in c
+                ],
+            }
+        )
+    root = [*newRoot]
+    count += len(root)
+
+    i = 0
+    while i < level:
+        i += 1
+        if len(root) <= intent_num:
+            return [{key: node[key] for key in node if key != "vector"} for node in root]
+        newRoot = []
+        hc_tree = clusterGenerator.hierarcy_clustering(root, distance_threshold)
+        for key, c in hc_tree.items():
+            if len(c) == 1:
+                newRoot.append(root[c[0]])
+            else:
+                intentsCluster = [root[index]["intent"] for index in c]
+                intent = await extractModelCluster.invoke(intentsCluster)
+                intent_v = model.embedding(intent, ["intent"])
+                newRoot.append(
+                    {
+                        "id": count + int(key),
+                        "intent": intent['intent'],
+                        "vector": intent_v,
+                        "priority": 1,
+                        "child_num": len(c),
+                        "child": [
+                            {
+                                key: root[index][key]
+                                for key in root[index]
+                                if key != "vector"
+                            }
+                            for index in c
+                        ],
+                    }
+                )
+        root = [*newRoot]
+        count += len(root)
+
+    return [{key: node[key] for key in node if key != "vector"} for node in root]
