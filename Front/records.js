a2cacc91cb47795db30aeb669ab706084ed45b4e
jiaqi-xiao <594521354@qq.com>
2024-10-15 18:01:33 +0800

    test: show route in page

--- Front/records.js
+++ Front/records.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const index = parseInt(urlParams.get("index"));
 
@@ -37,5 +37,54 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("recordId").textContent = record.id;
         document.getElementById("placeFormattedAddress").textContent = record.extraGMLocationContext.PlaceFormattedAddress || "--";
         document.getElementById("placeEditorialSummary").textContent = record.extraGMLocationContext.PlaceEditorialSummary || "--";
+    }
+
+
+    // 添加地图相关功能
+    const showRouteButton = document.getElementById("showRoute");
+    const mapFrame = document.getElementById("mapFrame");
+
+    // 获取Google API密钥
+    const apiKey = await getGoogleAPIKey();
+
+    // 初始化地图
+    if (apiKey) {
+        updateMapSrc();
+    } else {
+        console.error('Google API Key is not set');
+    }
+
+    showRouteButton.addEventListener("click", () => {
+        if (apiKey) {
+            updateMapSrc();
+        } else {
+            console.error('Google API Key is not set');
+        }
+    });
+
+    function updateMapSrc() {
+        // DOC: https://developers.google.com/maps/documentation/embed/embedding-map?hl=zh_CN
+        const origin = encodeURIComponent(document.getElementById("origin").value);
+        const destination = encodeURIComponent(document.getElementById("destination").value);
+        const waypoints = encodeURIComponent(document.getElementById("waypoints").value);
+
+        const baseUrl = "https://www.google.com/maps/embed/v1/directions";
+
+        const newSrc = `${baseUrl}?key=${apiKey}&origin=${origin}&destination=${destination}&waypoints=${waypoints}&avoid=tolls|highways`;
+
+        mapFrame.src = newSrc;
+    }
+
+    function getGoogleAPIKey() {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get('googleApiKey', (data) => {
+                if (data.googleApiKey) {
+                    resolve(data.googleApiKey);
+                } else {
+                    console.log('Google API Key is not set');
+                    resolve(null);
+                }
+            });
+        });
     }
 });
