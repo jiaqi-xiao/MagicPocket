237aa6ca63f2827313f44e9f112ceb39ec71517c
Zehuan <wangzehuan2021@outlook.com>
2024-11-18 21:57:03 +0800

    feat: segment web content

--- Back/main.py
+++ Back/main.py
@@ -322,6 +322,13 @@ async def incremental_construct_intent(
                 newIntentTree[key] = group[indices]
     return {"item": newIntentTree}
 
+@app.post("/segment/")
+async def page_content_segment_vectorize(
+    page_content: str
+):
+    # 对网页内容进行分句
+    # 对每个句子进行向量化
+    return "vectorized sentences"
 
 @app.post("/cluster/")
 # async def direct_extract_intent(
